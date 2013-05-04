d2d1870f99e31f5c1182a4be6e1da1bdd151424d
NguyenVinhHung <magic_gladiator123456@yahoo.com>
2013-05-04 10:07:02 +0700

    new code

---
+++ assignment_COSC2440/out/production/assignment_COSC2440/create_pokemon_table.sql
@@ -0,0 +1,8 @@
+create table if not exists Pokemon (
+    PokemonId int primary key,
+    PokemonName text,
+    Damage int,
+    Health int,
+    Type int,
+    OwnerName text
+);
---
+++ assignment_COSC2440/src/create_account_table.sql
@@ -0,0 +1,4 @@
+create table if not exists Account(
+    Username text primary key,
+    Password text
+);
